8e3970e0973fb54274c315e6129c477369ad9a12
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-09-29 10:08:05 -0400

    Updated from Brython Server: 9/29/2015 10:08:04 AM

--- birthday.py
+++ birthday.py
@@ -31,3 +31,16 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+from datetime import datetime
+from calendar import month_name
+todaymonth = datetime.today().month
+todaydate = datetime.today().day
+
+name = input("Hello, what is your name? ")
+month = float(input("Hi "+name+", what is the name of the month you were born in? "))
+year = float(int(input("And what year were you born in, "+name+"? ")))
+day = float(int(input("And the day? ")))
+
+if month == "December":
+    print("You are a winter baby ")
+
